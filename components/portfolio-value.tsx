130c679d575f134577e9c9b3be8d2ad8f2a38e0c
Fayyaziii332 <54409264+Fayyaziii332@users.noreply.github.com>
2024-12-20 23:41:07 +0500

    md to siddebar

--- components/portfolio-value.tsx
+++ components/portfolio-value.tsx
@@ -9,14 +9,14 @@ interface PortfolioValueProps {
 
 export function PortfolioValue({ value }: PortfolioValueProps) {
   return (
-    <Card className="rounded-[10px] md:rounded-[20px] h-[69px] md:h-[120px] px-[15px] py-[10px] lg:px-[20px] lg:py-[15px]">
+    <Card className="rounded-[10px] lg:rounded-[20px] h-[69px] lg:h-[120px] px-[15px] py-[10px] lg:px-[20px] lg:py-[15px]">
       <CardHeader className="p-0">
         <CardTitle className="p-0 text-[10px] lg:text-xs text-black flex items-center justify-between font-semibold font-mono">
           <span>PORTFOLIO VALUE</span>
           <Icon fill={'#FFFFFF'} name={'rightCorner'} />
         </CardTitle>
       </CardHeader>
-      <CardContent className="p-0 mt-[8px] md:mt-[14px]">
+      <CardContent className="p-0 mt-[8px] lg:mt-[14px]">
         <div className="text-[18px] lg:text-5xl font-bold">{value}</div>
       </CardContent>
     </Card>
