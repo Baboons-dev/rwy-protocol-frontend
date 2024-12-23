29f3a6398a19652bd51c6b7841be56730cd6096b
laibawaqar <laibawaqark@gmail.com>
2024-12-23 19:00:37 +0500

    shallow link

--- components/navigation/nav-item.tsx
+++ components/navigation/nav-item.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import Icon from '../Icon';
-import { useRouter } from 'next/navigation';
 
 interface NavItemProps {
   href: string;
@@ -18,28 +17,25 @@ export function NavItem({
   badge,
   isActive,
 }: NavItemProps) {
-  const router = useRouter();
-
   return (
-    <div
-      style={{
-        color: isActive ? '#0B63FF' : '#000000',
-      }}
-      className={cn(
-        'flex font-semibold text-[14px] items-center space-x-3 px-[30px] py-2 rounded-lg mb-1 group hover:bg-primary/5',
-        disabled ? 'cursor-not-allowed' : 'cursor-pointer',
-      )}
-      onClick={() => {
-        if (!disabled) router.push(href);
-      }}
-    >
-      <Icon fill={isActive ? '#0B63FF' : '#000000'} name={name} />
-      <span>{name}</span>
-      {badge && (
-        <span className="ml-auto text-[10px] leading-tight bg-blue-100 text-blue-600 px-[8px] py-[5px] rounded-[5px] font-mono font-medium">
-          {badge}
-        </span>
-      )}
-    </div>
+    <Link href={href} passHref shallow>
+      <div
+        style={{
+          color: isActive ? '#0B63FF' : '#000000',
+        }}
+        className={cn(
+          'flex font-semibold text-[14px] items-center space-x-3 px-[30px] py-2 rounded-lg mb-1 group hover:bg-primary/5',
+          disabled ? 'cursor-not-allowed' : 'cursor-pointer',
+        )}
+      >
+        <Icon fill={isActive ? '#0B63FF' : '#000000'} name={name} />
+        <span>{name}</span>
+        {badge && (
+          <span className="ml-auto text-[10px] leading-tight bg-blue-100 text-blue-600 px-[8px] py-[5px] rounded-[5px] font-mono font-medium">
+            {badge}
+          </span>
+        )}
+      </div>
+    </Link>
   );
 }
