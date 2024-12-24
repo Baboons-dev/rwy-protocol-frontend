d9b953cc655358fda5df8f897cb717ec5d8cfce3
laibawaqar <laibawaqark@gmail.com>
2024-12-24 15:55:41 +0500

    add conditional sidebar links

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,32 +2,43 @@
 
 import { usePathname } from 'next/navigation';
 import { NavItem } from '@/components/navigation/nav-item';
-
-const navigation = [
-  { name: 'Dashboard', href: '/' },
-  { name: 'Stake', href: '/stake' },
-  { name: 'Portfolio', href: '/portfolio' },
-  { name: 'Farms', href: '/farms' },
-  { name: 'Nodes', href: '/nodes', badge: 'Soon', disabled: true },
-  { name: 'Governance', href: '/governance', badge: 'Soon', disabled: true },
-];
+import { useMemo } from 'react';
+import { useAuthStore } from '@/lib/store/use-store';
 
 export function Sidebar() {
-  const pathname = usePathname();
+  const { token } = useAuthStore();
+  const navigation = useMemo(
+    () => [
+      { name: 'Dashboard', href: '/' },
+      { name: 'Stake', href: '/stake', hide: !token },
+      { name: 'Portfolio', href: '/portfolio', hide: !token },
+      { name: 'Farms', href: '/farms' },
+      { name: 'Nodes', href: '/nodes', badge: 'Soon', disabled: true },
+      {
+        name: 'Governance',
+        href: '/governance',
+        badge: 'Soon',
+        disabled: true,
+      },
+    ],
+    [token],
+  );
 
   return (
     <div className="flex flex-col py-[50px] w-64 border border-[#EFF3F4] rounded-[20px] bg-card min-h-[calc(97vh-4rem)]">
       <nav className="flex-1 px-2">
-        {navigation.map((item) => (
-          <NavItem
-            key={item.name}
-            href={item.href}
-            name={item.name}
-            badge={item.badge}
-            disabled={item.disabled}
-            // isActive={pathname === item.href}
-          />
-        ))}
+        {navigation
+          .filter(({ hide }) => !hide)
+          .map((item) => (
+            <NavItem
+              key={item.name}
+              href={item.href}
+              name={item.name}
+              badge={item.badge}
+              disabled={item.disabled}
+              // isActive={pathname === item.href}
+            />
+          ))}
       </nav>
     </div>
   );
