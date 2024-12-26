990f0acae6911adf092330ff34a18675c05a6263
Fayyaziii332 <54409264+Fayyaziii332@users.noreply.github.com>
2024-12-26 21:22:10 +0500

    reevert

--- components/navigation/nav-item.tsx
+++ components/navigation/nav-item.tsx
@@ -1,6 +1,5 @@
 import { cn } from '@/lib/utils';
-// import Icon from '../Icon';
-// import { usePathname } from 'next/navigation';
+import Icon from '../Icon';
 import React, { useState } from 'react';
 import Link from 'next/link';
 
@@ -8,37 +7,37 @@ interface NavItemProps {
   href: string;
   name: string;
   badge?: string;
-  // isActive: boolean;
-  // setIsActive: (active: string) => void;
-  // disabled?: boolean;
+  isActive: boolean;
+  setIsActive: (active: string) => void;
+  disabled?: boolean;
 }
 
 export function NavItem({
   href,
   name,
-  // disabled,
-  // isActive,
-  // setIsActive,
+  disabled,
+  isActive,
+  setIsActive,
   badge,
 }: NavItemProps) {
   return (
     <Link
-      href={href}
+      href={disabled ? '#' : href}
       passHref
-      // onClick={() => {
-      //   if (!disabled) setIsActive(href);
-      // }}
+      onClick={() => {
+        if (!disabled) setIsActive(href);
+      }}
     >
       <div
         className={cn(
           'flex font-semibold text-[14px] items-center space-x-3 px-[30px] py-2 rounded-lg mb-1 group',
-          // disabled ? 'cursor-not-allowed' : 'hover:bg-primary/5 cursor-pointer',
+          disabled ? 'cursor-not-allowed' : 'hover:bg-primary/5 cursor-pointer',
         )}
-        // style={{
-        //   color: disabled ? 'gray' : isActive ? '#0B63FF' : '#000000',
-        // }}
+        style={{
+          color: disabled ? 'gray' : isActive ? '#0B63FF' : '#000000',
+        }}
       >
-        {/* <Icon fill={isActive ? '#0B63FF' : '#000000'} name={name} /> */}
+        <Icon fill={isActive ? '#0B63FF' : '#000000'} name={name} />
         <span>{name}</span>
         {badge && (
           <span className="ml-auto text-[10px] leading-tight bg-blue-100 text-blue-600 px-[8px] py-[5px] rounded-[5px] font-mono font-medium">
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-// import { usePathname } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { NavItem } from '@/components/navigation/nav-item';
 import { useMemo, useState } from 'react';
 import { useAuthStore } from '@/lib/store/use-store';
 
 export function Sidebar() {
   const { token } = useAuthStore();
-  // const pathname = usePathname();
-  // const [activeTab, setActiveTab] = useState<string>(pathname ?? '/');
+  const pathname = usePathname();
+  const [activeTab, setActiveTab] = useState<string>(pathname ?? '/');
 
   const navigation = useMemo(
     () => [
@@ -28,19 +28,19 @@ export function Sidebar() {
   );
 
   return (
-    <div className="flex fixed top-[5rem] left-[10px] bottom-0 w-[260px] flex-col py-[50px] w-64 border border-[#EFF3F4] rounded-[20px] bg-card min-h-[calc(97vh-4rem)]">
+    <div className="hidden lg:flex fixed top-[5rem] left-[10px] bottom-0 w-[260px] flex-col py-[50px] w-64 border border-[#EFF3F4] rounded-[20px] bg-card min-h-[calc(97vh-4rem)]">
       <nav className="flex-1 px-2">
         {navigation
-          // .filter(({ hide }) => !hide)
+          .filter(({ hide }) => !hide)
           .map((item) => (
             <NavItem
               key={item.name}
               href={item.href}
               name={item.name}
               badge={item.badge}
-              // disabled={item.disabled}
-              // isActive={activeTab === item.href}
-              // setIsActive={setActiveTab}
+              disabled={item.disabled}
+              isActive={activeTab === item.href}
+              setIsActive={setActiveTab}
             />
           ))}
       </nav>
